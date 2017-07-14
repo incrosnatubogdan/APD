a4e604801382a8acc6dd89e806e0e64f39f76dc7
incrosnatubogdan <incrosnatubogdan@gmail.com>
2017-07-14 11:02:01 +0300

    Update main.c

--- MatrixOpenMP/main.c
+++ MatrixOpenMP/main.c
@@ -1,55 +1,6 @@
 #include <omp.h>
 #include<stdio.h>
 
-/*#define n s
-#define p s
-#define m s
-
-
-float a[n][p];
-float b[p][m];
-float c[n][m];
-
-
-
-int main() {
-
-    unsigned long i,j,k;
-
-    for(i=0; i<n; i++)
-        for(k=0; k<p ; k++)
-            a[i][k]= rand();
-
-
-    for(k=0; k<p; k++)
-        for(j=0; j<m ; j++)
-            a[k][j]= rand();
-
-
-    for(i=0; i<n; i++)
-        for(j=0; j<m ; j++)
-            a[i][j]= rand();
-
-
-
-
-#pragma omp parallel shared(a,b,c) private(i,j,k) //a,c,b are shared among the threads & i,j,k are private to each threads
-{
-    #pragma omp schedule (static)
-     for(i=0; i<n; i++) {
-        for(j=0; j<m ; j++) {
-            for(k=0; k<p; k++) {
-                c[i][j]+= a[i][k] * b[k][j];
-
-            }
-        }
-     }
-
-    return 0;
-}
-*/
-
-
 
 float a[1000][1000];
 float b[1000][1000];
